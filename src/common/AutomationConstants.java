d2feec0e8df123d3a4f007879c48844d4c137e99
jay prakash <jay823001@gmail.com>
2019-03-29 09:29:27 +0000

    AutomationConstants.java edited online with Bitbucket

--- src/common/AutomationConstants.java
+++ src/common/AutomationConstants.java
@@ -5,7 +5,7 @@ public interface AutomationConstants {
 	public static final String CONFIG_PATH = "./config/";
 	public static final String CONFIG_FILE = "config.properties";
 	public static final String DRIVER_PATH="./Drivers/";
-	public static final String CHROME_FILE="chromedriver";
+	public static final String CHROME_FILE="chromedriver.exe";
 	public static final String GECKO_FILE="geckodriver.exe";
 	public static final String PHANTOM_FILE="phantomjs.exe";
 	public static final String CHROME_KEY="webdriver.chrome.driver";
