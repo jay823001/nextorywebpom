d832fcfd8210694bd074a136f001113407e8f303
jay <jay_prakash791@hotmail.com>
2019-03-30 17:28:44 +0530

    added

--- src/common/AutomationConstants.java
+++ src/common/AutomationConstants.java
@@ -6,7 +6,7 @@ public interface AutomationConstants {
 	public static final String CONFIG_FILE = "config.properties";
 	public static final String DRIVER_PATH="./Drivers/";
 	public static final String CHROME_FILE="chromedriver";
-	public static final String GECKO_FILE="geckodriver.exe";
+	public static final String GECKO_FILE="geckodriver";
 	public static final String PHANTOM_FILE="phantomjs.exe";
 	public static final String CHROME_KEY="webdriver.chrome.driver";
 	public static final String GECKO_KEY="webdriver.gecko.driver";
